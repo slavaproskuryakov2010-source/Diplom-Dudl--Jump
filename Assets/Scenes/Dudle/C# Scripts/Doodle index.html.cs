5d026345a6f1f4acc691b9c28834b3bc5055521c
slavaproskuryakov2010-source <slavaproskuryakov2010@gmail.com>
2026-06-10 20:55:59 +0500

    5

--- Assets/Scenes/Dudle/C# Scripts/Doodle index.html.cs
+++ Assets/Scenes/Dudle/C# Scripts/Doodle index.html.cs
@@ -1,2 +1,25 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Player : MonoBehaviour
+{
+  public float  speed = 5f;
+    private Rigidbody2D rb;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    [System.Obsolete]
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.velocity =  new Vector2(horizontalInput * speed, rb.velocity.y);
+
+        if (rb.velocity.x < 0)
+            gameObject.GetComponent<SpriteRenderer>().flipX=true;
+        if (rb.velocity.x > 0)
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+    }
+}
